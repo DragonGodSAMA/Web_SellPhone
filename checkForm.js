6fe6d47cd30186e758d080c00d59cddb08d9847a
PENG <pandainafterglow@qq.com>
2026-04-07 10:53:07 +0800

    judge register form

--- checkForm.js
+++ checkForm.js
@@ -4,9 +4,57 @@
 * For future usage: 'id' name needed
 * You can change the regular expression to satisfy your need.
 * update 17:47:59
+* update Apr 7th 09:54:35 new feature: adapted to Register.html
 */
-
 function checkForm()
+{
+    //'id' below should be adjusted flexibly
+    Name = document.getElementById("name").value;
+    Address = document.getElementById("address").value;
+    PhoneNum = document.getElementById("phone").value;
+    email = document.getElementById("email").value;
+    userN = document.getElementById("username").value;
+    passw = document.getElementById("password").value;
+    
+    patt1 = /^[a-zA-Z ]+$/;
+    patt2 = /^[a-zA-Z0-9 ]+$/;
+    patt3 = /^[0-9]{11}$/;
+    patt4 = /^[^@]+@[^@]+\.(cn|com)$/;
+    patt5 = /^[a-zA-Z0-9]{6,}$/;
+
+    if (!patt1.test(Name)) {
+        alert("Enter The Correct Name! example: Tom Smith");
+        return false;
+    }
+
+    if (!patt2.test(Address)) {
+        alert("Enter the correct address with only alphanumeric characters and space");
+        return false;
+    }
+
+    if (!patt3.test(PhoneNum)) {
+        alert("Enter the correct China Mainland phone number");
+        return false;
+    }
+
+    if (!patt4.test(email)) {
+        alert("Enter the correct email");
+        return false;
+    }
+
+    if (!patt5.test(userN)) {
+        alert("Enter the correct username");
+        return false;
+    }
+
+    if (!patt5.test(passw)) {
+        alert("Enter the correct password");
+        return false;
+    }
+}
+
+
+function checkForm_ori()
 {
     //'id' below should be adjusted flexibly
     firstN = document.getElementById("fn").value; 
